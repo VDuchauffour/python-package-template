ada5741e2b30fef141bb69f7562007400e6b591c
VDuchauffour <vincent.duchauffour@proton.me>
2023-01-20 17:37:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="package",
+    version="0.0.1",
+    description="",
+    author="",
+    author_email="",
+    url="",
+    install_requires=[],
+    packages=find_packages(),
+)
