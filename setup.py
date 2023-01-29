9bfb40d8ff1cba5d2a0c0fcfed33fd0a57c3d1b6
VDuchauffour <vincent.duchauffour@proton.me>
2023-01-30 00:46:10 +0100

    Add dev dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import find_packages, setup
 
+DEV_REQUIREMENTS = open("requirements-dev.txt").read().strip().split()
+EXTRAS = {"dev": DEV_REQUIREMENTS}
+
 setup(
     name="package",
     version="0.0.1",
@@ -24,6 +27,6 @@ setup(
         ]
     ),
     install_requires=[],
-    extras_require={},
+    extras_require=EXTRAS,
     entry_points={"console_scripts": []},
 )
