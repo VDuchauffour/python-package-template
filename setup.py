982e78d4c9e5a828a17f01bec2337fce77310fd3
VDuchauffour <vincent.duchauffour@proton.me>
2023-02-10 21:31:23 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,34 @@
 
 from setuptools import find_packages, setup
 
+
+def read_requirements(
+    path: str,
+) -> list[str]:
+    """
+    Return the PyPI packages contains in a `requirements.txt` given his `path`.
+
+    Args:
+        path (str): Path of the file.
+
+    Returns:
+        list[str]: Return a list of packages.
+    """
+    try:
+        with open(path) as f:
+            return [
+                line.strip().split("=")[0]
+                for line in f.readlines()
+                if not line.startswith(("-", "--", "#"))
+            ]
+    except FileNotFoundError:
+        return []
+
+
+REQUIREMENTS = read_requirements("requirements.txt")
+DEV_REQUIREMENTS = read_requirements("requirements-dev.txt")
+EXTRAS = {"dev": DEV_REQUIREMENTS}
+
 EXCLUDE_DIRS = [
     "test",
     "tests",
@@ -15,13 +43,6 @@ EXCLUDE_DIRS = [
     "media",
 ]
 
-try:
-    DEV_REQUIREMENTS = open("requirements-dev.txt").read().strip().split()
-except FileNotFoundError:
-    DEV_REQUIREMENTS = []
-
-EXTRAS = {"dev": DEV_REQUIREMENTS}
-
 setup(
     name="package",
     version="0.0.1",
@@ -32,7 +53,7 @@ setup(
     packages=find_packages(
         exclude=EXCLUDE_DIRS,
     ),
-    install_requires=[],
+    install_requires=REQUIREMENTS,
     extras_require=EXTRAS,
     entry_points={"console_scripts": []},
 )
